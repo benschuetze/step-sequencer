bba159c53de962cceba7be6e61c524dd3fa780fb
Schuetze <Benjamin.Schuetze@scheer-group.com>
2023-11-15 20:41:17 +0100

    added bpm input (buggy af)

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
+import { useState } from "react";
 import "./App.css";
 import Sequencer from "./components/Sequencer";
 
 function App() {
+  const [bpm, setBpm] = useState(120);
   return (
     <>
+      <div className="flex items-center justify-center">
+        <input
+          className="mx-auto text-center"
+          type="number"
+          value={bpm || ""}
+          // oninput={updateValue(this.value)}
+          onChange={(e) => {
+            const value = e.target.value;
+            const parsedValue = parseInt(value) || 0;
+            if (parsedValue >= 0) setBpm(parsedValue);
+          }}
+        />
+      </div>
       <div className="w-screen h-screen flex items-center justify-center">
-        <Sequencer />
+        <Sequencer bpm={bpm} />
       </div>
     </>
   );
--- src/components/Sequencer.tsx
+++ src/components/Sequencer.tsx
@@ -1,42 +1,81 @@
 import { useState, useEffect } from "react";
 
-const Sequencer = () => {
-  const [steps, setSteps] = useState<Array<number>>([1, 2, 3, 4]);
+type SequencerProps = {
+  bpm: number;
+};
+
+const Sequencer = ({ bpm }: SequencerProps) => {
+  const [steps, setSteps] = useState<Array<number>>([
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16,
+  ]);
+  const [activatedSteps, setActivatedSteps] = useState<Array<number>>([]);
   const [currentActiveStep, setCurrentActiveStep] = useState<number>(0);
   const [playerAction, setPlayerAction] = useState<string>("Start");
   const [timeoutId, setTimeoutId] = useState<number | undefined>(undefined);
-
-  const interval = 60000 / 120;
+  const [isFirstRender, setIsFirstRender] = useState(true);
 
   const playSequence = () => {
-    setCurrentActiveStep((prev) => (prev === 4 ? 1 : prev + 1));
-    setTimeoutId(setTimeout(playSequence, interval));
+    console.log("BPM: ", bpm);
+    if (bpm > 0) {
+      const interval = 60000 / bpm / 4;
+      setCurrentActiveStep((prev) => (prev === 16 ? 1 : prev + 1));
+      setTimeoutId(setTimeout(playSequence, interval));
+    }
   };
 
   const handleStartStop = () => {
-    if (playerAction === "Start") {
+    if (playerAction === "Start" && bpm > 0) {
       clearTimeout(timeoutId);
       playSequence();
     } else {
       clearTimeout(timeoutId);
+      setCurrentActiveStep(0);
     }
     setPlayerAction((prev) => (prev === "Start" ? "Stop" : "Start"));
   };
 
+  const handleActivatedSteps = (step: number) => {
+    setActivatedSteps((prev) => {
+      console.log("includes step: ", prev.includes(step));
+      if (prev.includes(step)) {
+        const updatedSteps = [...prev];
+        updatedSteps.splice(updatedSteps.indexOf(step), 1);
+        console.log("UPDATED STEPS:", updatedSteps);
+        return updatedSteps;
+      } else {
+        return [...prev, step];
+      }
+    });
+  };
+
+  // use effect der ausgeführt wirtd wenn bpm sich ändert und in dem die aktuelle timeout id gecleart wird und an der selben stelle im player play sequence nochmal ausgeführt wird
+
   useEffect(() => {
-    console.log(currentActiveStep);
-  }, [currentActiveStep]);
+    console.log("BPM in SEQUENCER: ", bpm);
+    if (isFirstRender) {
+      setIsFirstRender(false);
+    } else {
+      console.log(bpm);
+      if (bpm > 0 && playerAction === "Stop") {
+        clearTimeout(timeoutId);
+        playSequence();
+      }
+    }
+  }, [bpm]);
 
   return (
     <>
       <button onClick={handleStartStop}>{playerAction}</button>
-      <div className="grid grid-cols-4 gap-2">
-        {steps.map((step, index) => (
+      <div className="grid grid-cols-16 gap-2">
+        {steps.map((step) => (
           <div
-            key={index}
-            className={`border border-black  rounded px-2 py-1 cursor-pointer hover:bg-slate-300 ${
-              step === currentActiveStep && "bg-black"
+            key={step}
+            className={`border border-black  rounded px-2 py-1 cursor-pointer  bg ${
+              step === currentActiveStep && "bg-red-400"
+            } ${
+              activatedSteps.includes(step) ? "bg-black" : "hover:bg-slate-300"
             }`}
+            onClick={() => handleActivatedSteps(step)}
           >
             O
           </div>
