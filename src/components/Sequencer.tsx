5102ea68e9ec334847d66ef437dfe8ebe423ffc1
Schuetze <Benjamin.Schuetze@scheer-group.com>
2023-11-16 14:25:54 +0100

    added supabase

--- src/components/Sequencer.tsx
+++ src/components/Sequencer.tsx
@@ -1,8 +1,44 @@
 import { useState, useEffect } from "react";
+import { createClient } from "@supabase/supabase-js";
 
 type SequencerProps = {
   bpm: number;
 };
+
+const API_KEY = import.meta.env.VITE_SUPABASE_API_KEY;
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_PROJECT_URL;
+
+const supabase = createClient(SUPABASE_URL, API_KEY);
+
+const getSamples = async () => {
+  try {
+    const { data, error } = await supabase.storage.from("kicks").list();
+    if (error) {
+      console.error("Error fetching bucket content:", error.message);
+    } else {
+      const signedUrlsResponses = data.map(async (sample) => {
+        const { data: urlData, error: signError } = await supabase.storage
+          .from("kicks")
+          .createSignedUrl(sample.name, 60);
+        if (signError) {
+          console.error(
+            `Error creating signed URL for ${sample.name}:`,
+            signError.message
+          );
+          return null;
+        } else {
+          return { name: sample.name, signedUrl: urlData.signedUrl };
+        }
+      });
+      const signedUrls = await Promise.all(signedUrlsResponses);
+      console.log("SIGNED URLSSS: ", signedUrls);
+    }
+  } catch (e) {
+    console.error("SUPABASE ERROR: ", e);
+  }
+};
+
+const Audiocontext = new AudioContext();
 
 const Sequencer = ({ bpm }: SequencerProps) => {
   const [steps, setSteps] = useState<Array<number>>([
@@ -14,11 +50,18 @@ const Sequencer = ({ bpm }: SequencerProps) => {
   const [timeoutId, setTimeoutId] = useState<number | undefined>(undefined);
   const [isFirstRender, setIsFirstRender] = useState(true);
 
-  const playSequence = () => {
-    console.log("BPM: ", bpm);
+  const playSequence = (bpmChangeWhilePlaying = false) => {
     if (bpm > 0) {
       const interval = 60000 / bpm / 4;
-      setCurrentActiveStep((prev) => (prev === 16 ? 1 : prev + 1));
+      setCurrentActiveStep((prev) => {
+        let step: number;
+        if (bpmChangeWhilePlaying) {
+          step = prev;
+        } else {
+          prev === 16 ? (step = 1) : (step = prev + 1);
+        }
+        return step;
+      });
       setTimeoutId(setTimeout(playSequence, interval));
     }
   };
@@ -36,11 +79,9 @@ const Sequencer = ({ bpm }: SequencerProps) => {
 
   const handleActivatedSteps = (step: number) => {
     setActivatedSteps((prev) => {
-      console.log("includes step: ", prev.includes(step));
       if (prev.includes(step)) {
         const updatedSteps = [...prev];
         updatedSteps.splice(updatedSteps.indexOf(step), 1);
-        console.log("UPDATED STEPS:", updatedSteps);
         return updatedSteps;
       } else {
         return [...prev, step];
@@ -48,38 +89,39 @@ const Sequencer = ({ bpm }: SequencerProps) => {
     });
   };
 
-  // use effect der ausgeführt wirtd wenn bpm sich ändert und in dem die aktuelle timeout id gecleart wird und an der selben stelle im player play sequence nochmal ausgeführt wird
-
   useEffect(() => {
-    console.log("BPM in SEQUENCER: ", bpm);
     if (isFirstRender) {
       setIsFirstRender(false);
     } else {
-      console.log(bpm);
       if (bpm > 0 && playerAction === "Stop") {
         clearTimeout(timeoutId);
-        playSequence();
+        playSequence(true);
       }
     }
   }, [bpm]);
+
+  useEffect(() => {
+    getSamples();
+  }, []);
 
   return (
     <>
       <button onClick={handleStartStop}>{playerAction}</button>
       <div className="grid grid-cols-16 gap-2">
-        {steps.map((step) => (
-          <div
-            key={step}
-            className={`border border-black  rounded px-2 py-1 cursor-pointer  bg ${
-              step === currentActiveStep && "bg-red-400"
-            } ${
-              activatedSteps.includes(step) ? "bg-black" : "hover:bg-slate-300"
-            }`}
-            onClick={() => handleActivatedSteps(step)}
-          >
-            O
-          </div>
-        ))}
+        {steps.map((step) => {
+          const stepIsActive = activatedSteps.includes(step);
+          return (
+            <div
+              key={step}
+              className={`border border-black rounded px-2 py-1 cursor-pointer bg ${
+                step === currentActiveStep ? "bg-red-400" : ""
+              } ${stepIsActive ? "bg-black" : "hover:bg-slate-300"}`}
+              onClick={() => handleActivatedSteps(step)}
+            >
+              O
+            </div>
+          );
+        })}
       </div>
     </>
   );
